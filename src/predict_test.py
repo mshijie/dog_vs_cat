2f797a2be81d77f4f2b59dc093600ef58be9452a
zhongxin <zhongxin@tmall.com>
2017-04-19 15:44:11 +0800

    add feature_extract.py

---
+++ src/predict_test.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+test_features = np.load("test_features.npy")
---
+++ src/train_model.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+train_features = np.load("train_features.npy")
+train_labels = np.load("train_labels.npy")
+
