59999921fee8e304a189aab773487b909036ebb8
zhongxin <zhongxin@tmall.com>
2017-04-19 22:23:15 +0800

    add train_model2.py

--- src/train_model2.py
+++ src/train_model2.py
@@ -1,17 +1,28 @@
 import numpy as np
 from keras.applications.vgg19 import VGG19
-from keras.optimizers import SGD
+from keras.layers import Dense
+from keras.models import Model
 
-TRAIN_SIZE = 10
+TRAIN_SIZE = 25000
 IMAGE_SIZE = 224
 
 train_images = np.memmap("train_images.npy", dtype='float32', mode='r', shape=(TRAIN_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
 train_labels = np.memmap("train_labels.npy", dtype="int32", mode='r+', shape=(TRAIN_SIZE,))
 
-model = VGG19(weights='imagenet', include_top=True)
-model.compile(optimizer=SGD(lr=0.01), loss='binary_crossentropy', metrics=['accuracy'])
+base_model = VGG19(weights=None, include_top=True)
 
-model.fit(train_images, train_labels, batch_size=128, nb_epoch=30, validation_split=0.2, verbose=1)
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = base_model.get_layer("fc2").output
+x = Dense(1024, activation='relu', name="more_fc1")(x)
+x = Dense(1024, activation='relu', name="more_fc2")(x)
+predictions = Dense(1, activation='sigmoid', name="predictions")(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+
+model.fit(train_images, train_labels, batch_size=128, epochs=30, validation_split=0.2, verbose=1)
 
 json = model.to_json()
 with open('model2.json', 'w') as f:
