208d06ef7f734fa38d048802d38d69b7882468c3
zhongxin <zhongxin@tmall.com>
2017-04-20 01:51:07 +0800

    mmap_mode

--- src/train_model.py
+++ src/train_model.py
@@ -1,14 +1,13 @@
 import os
-import pickle
 
+from keras.applications.inception_v3 import InceptionV3
 from keras.layers import *
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 from keras.optimizers import SGD
-from keras.applications.inception_v3 import InceptionV3
-from keras.layers import Dense, GlobalAveragePooling2D
 
 # create model
-base_model = InceptionV3(weights=None, include_top=False)
+base_model = InceptionV3(weights="imagenet", include_top=False)
 x = base_model.output
 x = GlobalAveragePooling2D(name='avg_pool')(x)
 predictions = Dense(1, activation='sigmoid', name="predictions")(x)
@@ -18,7 +17,7 @@ model.compile(optimizer=SGD(lr=0.005), loss='binary_crossentropy', metrics=['acc
 # train model
 images = np.load("processed_data/train/images.npy", mmap_mode="r")
 labels = np.load("processed_data/train/labels.npy")
-model.fit(images, labels, batch_size=64, epochs=1, verbose=1, validation_split=0.2)
+model.fit(images, labels, batch_size=32, epochs=1, verbose=1, validation_split=0.2)
 
 # save model
 os.makedirs("model", exist_ok=True)
